6818ec6aeb9189b8939655d485f2bb300baee218
Wendar Lin <wdlin@csie.nctu.edu.tw>
2024-06-20 10:58:41 +0800

    matrixSelection.pl reading both TSV & CSV

--- scripts/matrixSelection.pl
+++ scripts/matrixSelection.pl
@@ -50,14 +50,44 @@ for my $x (keys %target){
     open($outFILE{$x},">$outPrefix.$x");
 }
 
+# detect delimiter inside the source matrix file
+open(FILE,"<$srcMtxFilename");
+my $line1 = <FILE>;
+my $line2 = <FILE>;
+# set delimiter as \t and check
+my $delimiter = "\t";
+my $delimiterFlag = 0;
+if(not $delimiterFlag){
+    my @list1 = split(/$delimiter/,$line1);
+    my @list2 = split(/$delimiter/,$line2);
+    my $cnt1 = @list1;
+    my $cnt2 = @list2;
+    if($cnt1>1 && $cnt2>1 && $cnt1==$cnt2){
+        $delimiterFlag=1;
+    }
+}
+# set delimiter as , and check, if not pass
+if(not $delimiterFlag){
+    $delimiter = ",";
+    my @list1 = split(/$delimiter/,$line1);
+    my @list2 = split(/$delimiter/,$line2);
+    my $cnt1 = @list1;
+    my $cnt2 = @list2;
+    if($cnt1>1 && $cnt2>1 && $cnt1==$cnt2){
+        $delimiterFlag=1;
+    }
+}
+close FILE;
+
 # read source matrix
 open(FILE,"<$srcMtxFilename");
 # read first line
 $line=<FILE>;
 chomp $line;
-@s=split(/\t/,$line);
+@s=split(/$delimiter/,$line);
 $headerCnt = @s;
 for(my $i=0;$i<@s;$i++){
+    $s[$i]=~s/^"|"$//g; # remove possible quote signs from the two ends
     if($i==0){ # print to all files
         for my $x (keys %target){
             print {$outFILE{$x}} "$s[$i]";
@@ -77,9 +107,10 @@ for my $x (keys %target){
 # rest lines
 while($line=<FILE>){
     chomp $line;
-    @s=split(/\t/,$line);
+    @s=split(/$delimiter/,$line);
     die "token count inconsistent in $srcMtxFilename at row '$s[0]...'\n" if @s != $headerCnt;
     for(my $i=0;$i<@s;$i++){
+        $s[$i]=~s/^"|"$//g; # remove possible quote signs from the two ends
         if($i==0){ # print to all files
             for my $x (keys %target){
                 print {$outFILE{$x}} "$s[$i]";
